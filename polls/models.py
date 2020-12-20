508cf82213ce2c14bcccfd0abe9677aa3c9ff653
awemulya <awemulya@gmail.com>
2020-12-20 10:14:19 +0545

    add circleci

--- polls/models.py
+++ polls/models.py
@@ -8,6 +8,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    class Meta:
+        ordering = ["pub_date"]
+
     def __str__(self):
         return self.question_text
 
@@ -17,9 +20,53 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="choices"
+    )
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "blog_blog"
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    class Meta:
+        db_table = "blog_author"
+        ordering = ["name", "email"]
+
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    number_of_comments = models.IntegerField()
+    number_of_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    class Meta:
+        db_table = "blog_entry"
+        ordering = ["-pub_date", "-mod_date", "headline"]
+
+    def __str__(self):
+        return self.headline
